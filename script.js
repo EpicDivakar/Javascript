719637fd2659695067a5a0a42943f638614279d6
Diwakar Verma <88948065+EpicDivakar@users.noreply.github.com>
2025-06-02 21:23:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,88 @@
-//Script Mode is special mode in JS which help us to write bug free code//
-'use strict';
-let hasDriversLicense = false;
-const passTest = true;
-if(passTest) hasDriversLicense=true;
-if (hasDriversLicense) console.log("Yes He can drive the car");
+let js = "amazing";
+//if (js ==="amazing")alert("Javascript is fun");//
 
-//Functions in Javascript //
-//A peice of code that can be used over and over in the code//
+23+4+6+7;
+console.log(23+44+5+6);
 
-function logger(){//Logger is function name//
-    console.log("My name is Diwakar");//This is function body//
-}
-logger();//This is function invocation//
-logger();//This is function invocation//
-logger();//This is function invocation//
-logger();//This is function invocation//
-logger();//This is function invocation//
+console.log("JOnas");
+console.log(23); 
+console.log(64-86);
 
-function fruitProcessor(apples , oranges){
-   // console.log(apples , oranges);
-    const juice =`Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
-}
-// const applejuice=fruitProcessor(5,0)
-// console.log(applejuice);
-console.log(fruitProcessor(5,0))
-const appleorangejuice=fruitProcessor(2,4);
-console.log(appleorangejuice);
-const orangeAppleJuice=fruitProcessor(0,8);
-console.log(orangeAppleJuice);
+//Values and Variables
+let firstName = "Jonas"; // Writting small and than capital alphabets while nameing varible is known as camel casing//
 
-//Function Declaration and Expression
-//Function Declaration
-function calcAge1(birthYear){
-    /*nst age = 2037-birthYear;
-    return age;*/
-    return 2037-birthYear;
-}
- const age1 =calcAge1(1991);
- console.log(age1);
- //Function Expression
- const calcAge2=function (birthYear){
-    return 2037-birthYear;
- }
- const age2 = calcAge2(1991);
-console.log(age1,age2);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
-// Arrow Function//
-// Arrow Function Example//
-  const calcAge3 =birthYear => 2045-birthYear
-   const Age3=calcAge3(1990);
-   console.log(Age3);
-//Arrow function should only be used when we only have one parameter//
+let jonas_matilda = "JM";
+let name = "Jonas"; // This is a varible which is a rese44rved keyword and we  should nt use this as a varible name//
+let PI = 3.1415;//This is a constant varible and we shoulkd not change this value //
 
-//Function Calling another function in javascript//
-function cutfruitpieces(fruit){
-    return fruit *5;
-}
-function fruitProcessor(apples , oranges){
-    const applejuice = cutfruitpieces(apples);
-    const orangejuice = cutfruitpieces(oranges);
-     const juice =`Juice with ${applejuice} peice of apples and ${orangejuice} pieces of oranges.`;
-     return juice;
- }
- console.log(fruitProcessor(5,10))
+// Our variable name should be discriptive it should not be something which is not related to the actual thing//
+let myFirstJob = " Programmer";
+let myCurrentJob = "Teacher";
 
- //Its very common to call one function to another function//
- //Here we learn about we should not call a function twice//
- 
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+// Now if we need to change the value of the any varible than it should be like this only//
+myFirstJob = "Racer";
+console.log(myFirstJob);
+
+// Assignment for Values and variables//
+ let continent = "Asia";
+ let country = "India"
+ let population = 5;
+
+ console.log(continent);
+ console.log(country);
+ console.log(population);
+
+ // Data types in javascript//
+
+ let myAge = 23; // Number data type//
+ let myName = "Diwakar"; // String data type//
+
+ let javascriptIsFun = true;
+ console.log(javascriptIsFun);
+
+ console.log(typeof myAge);
+ console.log(typeof "Diwakar");
+ console.log(typeof javascriptIsFun);
+
+ javascriptIsFun = 'YES!';
+
+ let year;
+ console.log(year);
+ console.log(typeof year); // Undefined Data Types//
+
+ year = 1991;
+ console.log(typeof year);
+
+ console.log(typeof null);
+
+ // Let , Const and Var in Javascript//
+
+ let age = 30;
+ age = 31; // We can change the value and in technical term which means we mutated the variable//
+
+
+ // Const is used to declare a constant variable which means we can not change the value of the variable once it is declared //
+ // Immmutable Variable//
+   let birthYear = 1991;
+   console.log(birthYear);
+
+// var is pretty much like let but it has some differences which we will see later//
+
+var job1 = "Dancer";
+job1 = "Singer"; // We can change the value of the varible which is declared with var variable//
+
+// Operators in Javascript//
+
+let ageJonas = 2037-1991;
+let ageSarah = 2037 - 2018;
+console.log(ageJonas , ageSarah);
 
 
 
