7ff35b6b669364c70a0c23ace0782b566a0b2a9d
Diwakar Verma <88948065+EpicDivakar@users.noreply.github.com>
2025-06-09 23:17:43 +0530

    Update fundamental-2.js

--- fundamental-2.js
+++ fundamental-2.js
@@ -1,6 +1,6 @@
-'use strict';
+/*'use strict';
 
-/*
+
 ///////////////////////////////////////
 // Activating Strict Mode
 let hasDriversLicense = false;
@@ -12,102 +12,126 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const interface = 'Audio';
 // const private = 534;
 
-
-///////////////////////////////////////
 // Functions
 function logger() {
   console.log('My name is Jonas');
 }
+// Calling / running / invokind the function
+logger();
+logger();
+logger();
+logger();
 
-// calling / running / invoking function
-logger();
-logger();
-logger();
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+function fruitProcessor( apples , oranges){
+  console.log(apples , oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
   return juice;
 }
-
-const appleJuice = fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5,0);
 console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// Function Declaration
+const age1 = calcAge1(1991);
+function calcAge1(birthYear){
+  const age = 2023 - birthYear; // return 2023-birthYear;
+  return age;
+}
 
-const num = Number('23');
+console.log(age1);
 
+//Function Expression
 
-///////////////////////////////////////
-// Function Declarations vs. Expressions
-
-// Function declaration
-function calcAge1(birthYeah) {
-  return 2037 - birthYeah;
+const calcAge2 = function calcAge1(birthYear){
+  const age = 2023 - birthYear; // return 2023-birthYear;
+  return age;
 }
 const age1 = calcAge1(1991);
-
-// Function expression
-const calcAge2 = function (birthYeah) {
-  return 2037 - birthYeah;
-}
-const age2 = calcAge2(1991);
-
-console.log(age1, age2);
+console.log(age1);
+// One difference between function declaration and function expression is we can actually call them even before defining them.
 
 
-///////////////////////////////////////
-// Arrow functions
+// Arrow Function 
+// They are faster when runned
+// They are shorter to write
 
-const calcAge3 = birthYeah => 2037 - birthYeah;
-const age3 = calcAge3(1991);
-console.log(age3);
+ const calAge3 = birthYear => 2037 - birthYear;
+ const age3 = calAge3(1991);
+ console.log(age3);
 
-const yearsUntilRetirement = (birthYeah, firstName) => {
-  const age = 2037 - birthYeah;
-  const retirement = 65 - age;
-  // return retirement;
-  return `${firstName} retires in ${retirement} years`;
-}
+ yearsUntilRetirement = (birthYear , firstName) => {
+  const age = 2023 - birthYear;
+  const retirement= 65- age;
+  //return retirement;
+  return ``
+ }
+  console.log(yearsUntilRetirement(1991 , 'Jonas'));
+  console.log(yearsUntilRetirement(1991 , 'Jonas'));
 
-console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
-
-
-///////////////////////////////////////
-// Functions Calling Other Functions
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
-
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-
-  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
-console.log(fruitProcessor(2, 3));
-
-
-///////////////////////////////////////
-// Reviewing Functions
-const calcAge = function (birthYeah) {
-  return 2037 - birthYeah;
-}
-
-const yearsUntilRetirement = function (birthYeah, firstName) {
-  const age = calcAge(birthYeah);
-  const retirement = 65 - age;
-
-  if (retirement > 0) {
-    console.log(`${firstName} retires in ${retirement} years`);
-    return retirement;
-  } else {
-    console.log(`${firstName} has already retired 🎉`);
-    return -1;
+  const calcAge = function (birthYear){
+    return 2023 - birthYear;
+    } 
+    const yearsUntilRetirement = function (birthYear , firstName){
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
   }
-}
+  console.log(yearsUntilRetirement(1991 , 'jonas'));
 
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
-*/
+  // Coding Challenge #1
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+ 
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+ 
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+};
+ 
+checkWinner(scoreDolphins, scoreKoalas);
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+ 
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+ 
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+};
+ 
+checkWinner(scoreDolphins, scoreKoalas);   */
+
+
+// Array 
+const friend1 = 'Michel';
+const friend2 ='Diwakar';
+const friend3 = 'jyoti'; 
+
+const friends = ['Michel' , ' Diwakar' , 'Jyoti'];
+console.log(friends)
+
+const years = new Array(1991 , 1873 , 3782 );
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
